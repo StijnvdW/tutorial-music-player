04e26e31ce67dde9b4cdc00057b2c915d522bf98
Stijn <stijnvanderwielen@gmail.com>
2017-12-30 22:03:37 +0100

    Add autocomplete to search component.

--- app/components/search.component.js
+++ app/components/search.component.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Autocomplete from 'react-autocomplete';
 
 class Search extends React.Component {
     render() {
         return (
             <div className="search">
+                <Autocomplete
+                    ref="autocomplete"
+                    inputProps={{title: "Title"}}
+                    value={this.props.autoCompleteValue}
+                    items={this.props.tracks}
+                    getItemValue={(item) => item.title}
+                    onSelect={this.props.handleSelect}
+                    onChange={this.props.handleChange}
+                    renderItem={this.handleRenderItem.bind(this)}
+                />
             </div>
         );
     }
