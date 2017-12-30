9da9492d27515ed615cfabf5c444716183388181
Stijn <stijnvanderwielen@gmail.com>
2017-12-30 22:08:46 +0100

    Add handler to search component.

--- app/components/search.component.js
+++ app/components/search.component.js
@@ -2,6 +2,31 @@ import React from 'react';
 import Autocomplete from 'react-autocomplete';
 
 class Search extends React.Component {
+    handleRenderItem(item, isHighlighted){
+        const listStyles = {
+            item: {
+                padding: '2px 6px',
+                cursor: 'default'
+            },
+            highlightedItem: {
+                color: 'white',
+                background: '#F38B72',
+                padding: '2px 6px',
+                cursor: 'default'
+            }
+        };
+
+        return (
+            <div
+                style={isHighlighted ? listStyles.highlightedItem : listStyles.item}
+                key={item.id}
+                id={item.id}
+            >
+                {item.title}
+            </div>
+        )
+    }
+
     render() {
         return (
             <div className="search">
